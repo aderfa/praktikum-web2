bb9f04dc671271d14b8b2ce5ed410679e0d81b07
aderfa <77231874+aderfa@users.noreply.github.com>
2024-05-14 02:59:05 +0700

    Create 404.php

---
+++ pages/404.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col">
+        <div class="fs-4 mb-3">404</div>
+    </div>
+    <div class="col">
+        <a href="" class="btn btn-primary float-end"><i class="fa fa-arrow circle-left"></i> Kembali</a>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col">
+        <div class="alert alert-light" role="alert">Halaman Tidak ditemukan</div>
+    </div>
+</div>
