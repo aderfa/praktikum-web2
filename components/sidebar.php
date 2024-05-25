26c15555c2c44358bd249fbbf1e6fea15ba8f8a0
aderfa <77231874+aderfa@users.noreply.github.com>
2024-05-25 22:13:23 +0700

    Add files via upload

--- components/sidebar.php
+++ components/sidebar.php
@@ -1,10 +1,34 @@
 <div class="bg-white" id="sidebar-wrapper">
     <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom"><i class="fas fa-money-bill me-2"></i>Penggajian</div>
     <div class="list-group list-group-flush my-3">
-        <a href="?page=dashboard" class="list-group-item list-group-item-action bg-transparent second-text active"><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>
-        <a href="?page=bagian" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-building me-2"></i>Bagian</a>
-        <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users me-2"></i>Karyawan</a>
-        <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-money-bill me-2"></i>Penggajian</a>
+        <?php
+        
+            $page = $_GET['page'];
+          
+        if ($page == 'dashboard') {
+        echo '<a href="?page=dashboard" class="list-group-item list-group-item-action bg-transparent second-text active"><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>';
+        }else{
+           echo ' <a href="?page=dashboard" class="list-group-item list-group-item-action bg-transparent second-text "><i class="fas fa-tachometer-alt me-2"></i>Dashboard</a>';       
+        }
+        if ($page == 'bagian'){
+       echo  ' <a href="?page=bagian" class="list-group-item list-group-item-action bg-transparent second-text fw-bold active"><i class="fas fa-building me-2"></i>Bagian</a>';
+        }else{
+            echo  ' <a href="?page=bagian" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-building me-2"></i>Bagian</a>';  
+        }
+        if ($page == 'karyawan'){
+            echo '<a href="?page=karyawan" class="list-group-item list-group-item-action bg-transparent second-text fw-bold active"><i class="fas fa-users me-2"></i>Karyawan</a>';
+        }else{
+            echo '<a href="?page=karyawan" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-users me-2"></i>Karyawan</a>';
+        }
+        if ($page == 'penggajian'){
+            echo '<a href="?page=penggajian" class="list-group-item list-group-item-action bg-transparent second-text fw-bold active"><i class="fas fa-money-bill me-2"></i>Penggajian</a>';
+        }else{
+
+        echo '<a href="?page=penggajian" class="list-group-item list-group-item-action bg-transparent second-text fw-bold"><i class="fas fa-money-bill me-2"></i>Penggajian</a>';
+        }
+        ?>
+
         <a href="#" class="list-group-item list-group-item-action bg-transparent text-danger fw-bold"><i class="fas fa-power-off me-2"></i>Logout</a>
-    </div>
+        
+        </div>
 </div>
